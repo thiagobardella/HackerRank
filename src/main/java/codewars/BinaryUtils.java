2a6b7853d45f3cb3ea17a00b653dbdc7246f1777
unknown <thiago.bardella@gmail.com>
2019-04-13 17:39:00 -0300

    dummy test class

--- src/main/java/codewars/BinaryUtils.java
+++ src/main/java/codewars/BinaryUtils.java
@@ -17,8 +17,46 @@ public class BinaryUtils {
     }
 
     public static int toBinaryOptimal(int n) {
-
         String binaryString = Integer.toBinaryString(n);
         return Integer.parseUnsignedInt(binaryString);
+    }
+
+
+
+}
+
+ class HQ {
+    public static String HQ9(char code) {
+        switch (code) {
+            case 'H': {
+                return "Hello World!";
+            }
+            case 'Q': {
+                return "Q";
+            }
+            case '9': {
+                StringBuilder stringBuilder = new StringBuilder();
+                for(int i = 99; i >= 1; i--) {
+                    if (i==1) {
+                        stringBuilder.append(i).append(" bottle of beer on the wall, ").append(i).append(" bottle of beer.\n")
+                                .append("Take one down and pass it around, no more bottles of beer on the wall.\n");
+                    }
+                    else {
+                        stringBuilder.append(i).append(" bottles of beer on the wall, ").append(i).append(" bottles of beer.\n")
+                                .append("Take one down and pass it around, ");
+                                if (i==2) {
+                                    stringBuilder.append("1 bottle of beer on the wall.\n");
+                                }
+                                else {
+                                    stringBuilder.append(i - 1).append(" bottles of beer on the wall.\n");
+                                }
+                    }
+                }
+                stringBuilder.append("No more bottles of beer on the wall, no more bottles of beer.\n" +
+                        "Go to the store and buy some more, 99 bottles of beer on the wall.");
+                return stringBuilder.toString();
+            }
+            default: return null;
+        }
     }
 }
