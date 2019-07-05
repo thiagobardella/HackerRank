3f363428677b559229090b4fc57e0f4efb2b884b
unknown <thiago.bardella@gmail.com>
2019-07-05 08:26:35 -0300

    new wexercise

--- README.md
+++ README.md
@@ -17,6 +17,10 @@ https://www.codewars.com/kata/59d0ee709f0cbcf65400003b
 
 https://www.codewars.com/kata/meeting?utm_source=newsletter&utm_medium=email&utm_campaign=weekly_coding_challenges&utm_term=2019-06-25
 
+* Reverse or Rotate
+
+https://www.codewars.com/kata/56b5afb4ed1f6d5fb0000991
+
 * Scaling Squared Strings
 
 https://www.codewars.com/kata/56ed20a2c4e5d69155000301
@@ -24,7 +28,3 @@ https://www.codewars.com/kata/56ed20a2c4e5d69155000301
 * Set alarm
 
 https://www.codewars.com/kata/l1-set-alarm?utm_source=newsletter&utm_medium=email&utm_campaign=weekly_coding_challenges&utm_term=2019-06-25
-
-* Top Down Movement System
-
-https://www.codewars.com/kata/top-down-movement-system/train/java
---
+++ src/main/java/codewars/games/RevRot.java
@@ -0,0 +1,39 @@
+package codewars.games;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class RevRot {
+
+    public static String revRot(String strng, int sz) {
+
+        if (sz <= 0 || strng.isEmpty() || sz > strng.length()) return "";
+
+        List<String> strngAsList = Arrays.asList(strng.split(""));
+
+        return IntStream.range(0, strng.length()/sz - 1)
+                 .mapToObj(start -> strngAsList.subList(start*sz, sz))
+                 .map(chunk -> {
+                     if (sumDigitsCubes(chunk)/2 == 0) {
+                         StringBuilder stringBuilder = new StringBuilder();
+                         stringBuilder.append(chunk).reverse();
+                         return stringBuilder.toString();
+                     }
+                     return rotateLeft(1, chunk);
+                 })
+                 .collect(Collectors.joining());
+    }
+
+    public static int sumDigitsCubes(List<String> input) {
+        return input.stream().mapToInt(digit -> (int)Math.pow(Integer.valueOf(digit), 3)).sum();
+    }
+
+    public static String rotateLeft(int offset, List<String> input) {
+        String inputString = String.join("", input);
+        int i = offset % inputString.length();
+        return inputString.substring(i) + inputString.substring(0, i);
+
+    }
+}
